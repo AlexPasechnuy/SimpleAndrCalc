a2cb55472834678be1587f4b5cd7f55cf6328593
AlexPasechnuy <alexpasechnuy@gmail.com>
2019-10-06 17:44:21 +0300

    Functionality and constraints added

--- app/src/main/java/com/example/alex/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/alex/simplecalculator/MainActivity.java
@@ -14,14 +14,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onButtonClick(View v){
-//        EditText el1 = (EditText)findViewById(R.id.num1);
-//        EditText el2 = (EditText)findViewById(R.id.num2);
-//        TextView resText = (TextView)findViewById(R.id.result);
-//
-//        int num1 = Integer.parseInt(el1.getText().toString());
-//        int num2 = Integer.parseInt(el2.getText().toString());
-//        int resSum = num1 + num2;
-//        resText.setText(Integer.toString(resSum));
+    public void onPlusClick(View v){
+        EditText el1 = (EditText)findViewById(R.id.num1);
+        EditText el2 = (EditText)findViewById(R.id.num2);
+        TextView resText = (TextView)findViewById(R.id.result);
+
+        double num1 = Integer.parseInt(el1.getText().toString());
+        double num2 = Integer.parseInt(el2.getText().toString());
+        double resSum = num1 + num2;
+        resText.setText(Double.toString(resSum));
+    }
+
+    public void onMinusClick(View v){
+        EditText el1 = (EditText)findViewById(R.id.num1);
+        EditText el2 = (EditText)findViewById(R.id.num2);
+        TextView resText = (TextView)findViewById(R.id.result);
+
+        double num1 = Integer.parseInt(el1.getText().toString());
+        double num2 = Integer.parseInt(el2.getText().toString());
+        double resSum = num1 - num2;
+        resText.setText(Double.toString(resSum));
+    }
+
+    public void onMultClick(View v){
+        EditText el1 = (EditText)findViewById(R.id.num1);
+        EditText el2 = (EditText)findViewById(R.id.num2);
+        TextView resText = (TextView)findViewById(R.id.result);
+
+        double num1 = Integer.parseInt(el1.getText().toString());
+        double num2 = Integer.parseInt(el2.getText().toString());
+        double resSum = num1 * num2;
+        resText.setText(Double.toString(resSum));
+    }
+
+    public void onDivClick(View v){
+        EditText el1 = (EditText)findViewById(R.id.num1);
+        EditText el2 = (EditText)findViewById(R.id.num2);
+        TextView resText = (TextView)findViewById(R.id.result);
+
+        double num1 = Integer.parseInt(el1.getText().toString());
+        double num2 = Integer.parseInt(el2.getText().toString());
+        double resSum = num1/num2;
+        resText.setText(Double.toString(resSum));
+    }
+
+    public void onModClick(View v){
+        EditText el1 = (EditText)findViewById(R.id.num1);
+        EditText el2 = (EditText)findViewById(R.id.num2);
+        TextView resText = (TextView)findViewById(R.id.result);
+
+        double num1 = Integer.parseInt(el1.getText().toString());
+        double num2 = Integer.parseInt(el2.getText().toString());
+        double resSum = num1 % num2;
+        resText.setText(Double.toString(resSum));
+    }
+
+    public void onPowClick(View v){
+        EditText el1 = (EditText)findViewById(R.id.num1);
+        EditText el2 = (EditText)findViewById(R.id.num2);
+        TextView resText = (TextView)findViewById(R.id.result);
+
+        double num1 = Integer.parseInt(el1.getText().toString());
+        double num2 = Integer.parseInt(el2.getText().toString());
+        double resSum = Math.pow(num1, num2);
+        resText.setText(Double.toString(resSum));
     }
 }
